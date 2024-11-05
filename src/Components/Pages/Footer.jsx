5663540159d928835c6635cca3fe64db5cdedb2e
Fahad Alam <fahadalam12405@gmail.com>
2024-11-06 01:37:15 +0500

    change phone and adress

--- src/Components/Pages/Footer.jsx
+++ src/Components/Pages/Footer.jsx
@@ -13,31 +13,87 @@ export default function Footer() {
             <div className="col-lg-3 col-md-6">
               <h4 className="text-white mb-3">Quick Links</h4>
               <div className="d-flex flex-column">
-                <Link to="/Home" className="text-white mb-2">Home</Link>
-                <Link to="/About1" className="text-white mb-2">About</Link>
-                <Link to="/FAQ1" className="text-white mb-2">FAQ</Link>
-                <Link to="/contact" className="text-white mb-2">Contact</Link>
+                <Link to="/Home" className="text-white mb-2">
+                  Home
+                </Link>
+                <Link to="/About1" className="text-white mb-2">
+                  About
+                </Link>
+                <Link to="/FAQ1" className="text-white mb-2">
+                  FAQ
+                </Link>
+                <Link to="/contact" className="text-white mb-2">
+                  Contact
+                </Link>
               </div>
             </div>
-            
 
             <div className="col-lg-3 col-md-6">
               <h4 className="text-white mb-3">Contact</h4>
-              <p className="mb-2">
-                <i className="fa fa-map-marker-alt me-3" />
-                USA , FLorida, Miami
-              </p>
-              <p className="mb-2">
-                <i className="fa fa-phone-alt me-3" />
-                +000 000 000 0000
-              </p>
-              <p className="mb-2" style={{ display: 'flex', alignItems: 'center' }}>
-  <i className="fa fa-envelope" style={{ marginRight: '1.5rem' }} />
-  info@upstotrademarkrevive.com
-</p>
 
+              {/* Address link to open in Google Maps */}
+              <p className="mb-2">
+                <a
+                  href="https://www.google.com/maps/search/?api=1&query=11400+West+Olympic+Blvd,+Suite+200,+Los+Angeles,+CA+9006"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "none",
+                    display: "flex",
+                    alignItems: "flex-start",
+                  }}
+                >
+                  <i
+                    className="fa fa-map-marker-alt"
+                    style={{
+                      marginRight: "2rem",
+                      marginTop: "0.2rem",
+                      fontSize: "1.2rem",
+                    }}
+                  />
+                  <span style={{ lineHeight: "1.4" }}>
+                    11400 West Olympic Blvd,
+                    <br />
+                    Suite 200 Los Angeles, CA 9006
+                  </span>
+                </a>
+              </p>
 
+              {/* Phone link to open dialer */}
+              <p className="mb-2">
+                <a
+                  href="tel:+17148221238"
+                  style={{ color: "inherit", textDecoration: "none" }}
+                >
+                  <i className="fa fa-phone-alt me-3" />
+                  +1 714 822 1238
+                </a>
+              </p>
+
+              {/* Email link to open mail client */}
+              <p
+                className="mb-2"
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <a
+                  href="mailto:info@upstotrademarkrevive.com"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "none",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <i
+                    className="fa fa-envelope"
+                    style={{ marginRight: "1.5rem" }}
+                  />
+                  info@upstotrademarkrevive.com
+                </a>
+              </p>
             </div>
+
             <div className="col-lg-3 col-md-6">
               <h4 className="text-white mb-3">Gallery</h4>
               <div className="row g-2 pt-2">
@@ -88,7 +144,8 @@ export default function Footer() {
             <div className="col-lg-3 col-md-6">
               <h4 className="text-white mb-3">Stay Updated!</h4>
               <p>
-                Subscribe to our newsletter to receive the latest news, updates, and tips on trademark registration and protection.
+                Subscribe to our newsletter to receive the latest news, updates,
+                and tips on trademark registration and protection.
               </p>
 
               <div
@@ -113,16 +170,20 @@ export default function Footer() {
         <div className="container">
           <div className="copyright">
             <div className="row">
-            <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-  ©{" "}
-  <a className="border-bottom" href="https://usptotrademarkrevive.com" target="_blank" rel="noopener noreferrer">
-    TRADEMARK REVIVE &nbsp;
-  </a>
-  All Rights Reserved.
-  <br />
-  <br />
-</div>
-
+              <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                ©{" "}
+                <a
+                  className="border-bottom"
+                  href="https://usptotrademarkrevive.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  TRADEMARK REVIVE &nbsp;
+                </a>
+                All Rights Reserved.
+                <br />
+                <br />
+              </div>
 
               <div className="col-md-6 text-center text-md-end">
                 <div className="footer-menu">
@@ -131,7 +192,6 @@ export default function Footer() {
                   <Link to="/FAQ1">FAQ</Link>
                   <Link to="/contact">Contact</Link>
                 </div>
-
               </div>
             </div>
           </div>
